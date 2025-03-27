d690eb43de9129c86437e8eddd7f4759fe7a0c42
Jenny <jmontanor1800@alumno.ipn.mx>
2025-03-27 02:29:33 -0600

    Finalizado sin estilos B

--- empleados-app/src/app/components/empleado-list/empleado-list.component.ts
+++ empleados-app/src/app/components/empleado-list/empleado-list.component.ts
@@ -20,6 +20,7 @@ export class EmpleadoListComponent implements OnInit {
   empleados: any[] = [];
   puestos: any[] = [];
   mostrarFormulario: boolean = false;
+  empleadoSeleccionado: any = null;
 
   constructor(private fb: FormBuilder, private http: HttpClient) {}
 
@@ -40,15 +41,9 @@ export class EmpleadoListComponent implements OnInit {
       .get<any[]>(
         'https://67d0ea93825945773eb24739.mockapi.io/api/v1/getAllJobs'
       )
-      .subscribe(
-        (data) => {
-          console.log(data);
-          this.puestos = data;
-        },
-        (error) => {
-          console.error('Error al obtener los puestos:', error);
-        }
-      );
+      .subscribe((data) => {
+        this.puestos = data;
+      });
   }
 
   agregarEmpleado() {
@@ -59,7 +54,46 @@ export class EmpleadoListComponent implements OnInit {
       };
       this.empleados.push(nuevoEmpleado);
       this.empleadoForm.reset();
-      this.mostrarFormulario = false; // ✅ Ocultamos el formulario después de agregar
+      this.mostrarFormulario = false; // Ocultamos el formulario después de agregar
     }
+  }
+
+  editarEmpleado(empleado: any) {
+    this.empleadoSeleccionado = empleado;
+    this.empleadoForm.setValue({
+      nombre: empleado.nombre,
+      puesto: empleado.puesto,
+      fechaIngreso: empleado.fechaIngreso,
+      salario: empleado.salario,
+      edad: empleado.edad || '',
+    });
+    this.mostrarFormulario = true; //  Mostramos el formulario al editar
+  }
+
+  // Función para guardar los cambios en un empleado
+  actualizarEmpleado() {
+    if (this.empleadoForm.valid && this.empleadoSeleccionado) {
+      const empleadoIndex = this.empleados.findIndex(
+        (e) => e.id === this.empleadoSeleccionado.id
+      );
+      if (empleadoIndex !== -1) {
+        this.empleados[empleadoIndex] = {
+          ...this.empleados[empleadoIndex],
+          ...this.empleadoForm.value,
+        };
+        this.cancelarEdicion(); // Cancelamos la edición después de guardar
+      }
+    }
+  }
+
+  eliminarEmpleado(id: number) {
+    this.empleados = this.empleados.filter((empleado) => empleado.id !== id);
+  }
+
+  // Función para cancelar la edición
+  cancelarEdicion() {
+    this.empleadoForm.reset();
+    this.mostrarFormulario = false;
+    this.empleadoSeleccionado = null; // Limpiar el empleado seleccionado
   }
 }
